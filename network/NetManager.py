6e29321081d5f905c1e99768ba50b0d004711eea
Omer Derazon <omerderazon92@gmail.com>
2019-08-04 17:46:31 +0300

    Support Test Env

--- network/NetManager.py
+++ network/NetManager.py
@@ -6,19 +6,26 @@ from pyathenajdbc import connect
 import re
 
 
-class Enviroments(Enum):
+class Env(Enum):
     KCL_PREP = "kclprep"
+    KCL_TEST = "test"
 
 
 articfactory_base_url = 'http://aa-artifactory.intel.com:8081/artifactory/health-snapshot-local/com/intel/aa/validation_files/'
 
 
 def scan_athena(query, env):
-    if env == Enviroments.KCL_PREP.value:
+    if env == Env.KCL_PREP.value:
         conn = connect(profile_name='health-customers',
                        s3_staging_dir='s3://aws-athena-query-results-eu-west-1-036573440528/',
                        region_name='eu-west-1',
                        schema_name='kclprep')
+        return pd.read_sql(query, conn)
+    if env == Env.KCL_TEST.value:
+        conn = connect(profile_name='health',
+                       s3_staging_dir='s3://aws-athena-query-results-458907533143-us-west-2/',
+                       region_name='us-west-2',
+                       schema_name='test')
         return pd.read_sql(query, conn)
 
 
